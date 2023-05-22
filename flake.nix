25dfe2e01a1167cd166ff52dc57507cedc9d477c
wrvsrx <wrvsrx@outlook.com>
2023-05-22 10:24:28 +0800

    chore(lock): update flake.lock

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "flake lock tracker";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: { };
+}
